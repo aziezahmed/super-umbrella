fe7e204937e29cc66f541985cf0d83d311865fdb
Aziez Ahmed Chawdhary <aziezahmed@gmail.com>
2021-10-22 20:45:18 +0100

    revert to basic node template

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,15 @@
 import express from 'express';
-import cron from 'node-cron';
-import * as dotenv from 'dotenv';
 
-dotenv.config();
 const app = express();
-const port = 8080; // default port to listen
-const uri = process.env.MONGODB_URI;
+const port = 80; // default port to listen
 
 // define a route handler for the default home page
 app.get('/', (req, res) => {
-  console.info(uri);
   res.send('Hello world!');
-});
-
-// Schedule tasks to be run on the server.
-cron.schedule('* * * * *', () => {
-  console.log('running a task every minute');
 });
 
 // start the Express server
 app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`);
 });
+
